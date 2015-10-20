85f2a04cd9bbfc9bb7973460d7164d7dbdf29005
Rodi <DAM227@ACARBALLEIRA.LOCAL>
2015-10-20 10:42:49 +0200

    Cambiado Texto De Label

--- app/src/main/java/com/example/dam227/pruebagit/MainActivity.java
+++ app/src/main/java/com/example/dam227/pruebagit/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dam227.pruebagit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView)findViewById(R.id.textView);
+        tv.setText("Pene");
     }
 }
